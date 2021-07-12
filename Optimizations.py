b97bfc7cb78d65ad70b93049e67c12029e6a41d5
luicgoncalves <51790065+luicgoncalves@users.noreply.github.com>
2021-07-12 19:52:21 -0300

    Acrescimo de funções
    Acrescimo das funções de Golden Section Search e Aproximação Quadrática

--- Optimizations.py
+++ Optimizations.py
@@ -3,7 +3,6 @@ import math
 
 def bracket(x1, funcao, passo=0.000001, multiplicador=2):
 
-  intervalo = [x1]
 
   x2 = x1 + passo
   intervalo = [x1, x2]
@@ -40,26 +39,69 @@ def bracket(x1, funcao, passo=0.000001, multiplicador=2):
   return intervalo
 
 
-def golden_search(funcao, x1, x2, tolerancia=1e-6):
+def golden_search(f,a,b,tol=1e-6):
 
-    gr = (1 + math.sqrt(5))/2
+  gr = (1 + math.sqrt(5))/2
 
-    c = x2 - (x2-x1)/gr
-    d = x1 + (x2-x1)/gr
+  c = b - (b-a)/gr
+  d = a + (b-a)/gr
 
-    while abs(x2-x1) > tolerancia:
-        if funcao(c) > funcao(d):
-            x1 = c
-        else:
-            x2 = d
+  while abs(b-a) > tol:
+    if f(c) > f(d):
+      a = c
+    else:
+      b = d
       
-        c = x2 - (x2-x1)/gr
-        d = x1 + (x2-x1)/gr
+    c = b - (b-a)/gr
+    d = a + (b-a)/gr
 
-    return (x2+x1)/2
+  return (b+a)/2
+
+
+def quadratic_fit_search(f,a,b,tol=1e-6):
+
+  c = b
+  b = (c-a)/2
+
+  fa = f(a)
+  fb = f(b)
+  fc = f(c)
+
+  while abs(c-a) < tol:
+    x = 0.5*(fa*(b**2-c**2)+fb*(c**2-a**2)+fc*(a**2-b**2))/(fa*(b-c) +fb*(c-a) +fc*(a-b))
+    fx = f(x)
+    print(x)
+    if x > b:
+      if fx > fb:
+        c = x
+        fc = fx
+      else: 
+        a = b
+        fa = fb
+        b = x
+        fb = fx
+    else:
+      if fx > fb:
+        a = x
+        fa = fx
+      else:
+        c = b
+        fc = fb
+        b = x
+        fb = fx
+
+  return (a+c)/2
 
 f = lambda x: (x)**2
 
 
 print(bracket(1,f))
-print(golden_search(f,-5,10))
+
+
+
+
+
+#1º Passo - Gerar uma solução inicial X
+#2º Passo - Definir uma direção de busca S
+#3º Passo - Dar um passo  de amanho alpha na direção de S. X = X +alpha.S
+#4º Passo - Se não critério de parada ir para passo 2
